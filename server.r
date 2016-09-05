a521db49c87619d3f14a5f80996a639affbcb911
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2016-09-05 11:06:11 +0300

    Make it work

---
+++ server.r
@@ -0,0 +1,28 @@
+library(shiny)
+library(datasets)
+library(topicmodels)
+
+## load data
+
+## todo: generalize and clean up
+
+data = list()
+
+path = "/Users/mnelimar/Dropbox/projects/2016-topicmodelkritiikki/papereittain"
+files = list.files( path )
+
+for( f in files ) {
+    p <- paste( path , f, sep = '/' )
+    load( p )
+    data[ model@k ] <- model
+}
+
+## start server
+shinyServer(function(input, output) {
+
+  output$view <- renderTable({
+    model <- data[[ input$k ]]
+    terms( model, input$n )
+  })
+
+})
---
+++ ui.r
@@ -0,0 +1,37 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+
+  # Application title.
+  titlePanel("Topic model examination tool"),
+
+  # Sidebar with controls to select a dataset and specify the
+  # number of observations to view. The helpText function is
+  # also used to include clarifying text. Most notably, the
+  # inclusion of a submitButton defers the rendering of output
+  # until the user explicitly clicks the button (rather than
+  # doing it immediately when inputs change). This is useful if
+  # the computations required to render output are inordinately
+  # time-consuming.
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataset", "Choose a dataset:",
+                  choices = c("rock", "pressure", "cars")),
+
+      sliderInput("k", "Choose number of topics", 2, 50, 2 ),
+      sliderInput("n", "Choose number of words", 5, 30, 5, 5),
+
+      submitButton("Update View")
+    ),
+
+    # Show a summary of the dataset and an HTML table with the
+    # requested number of observations. Note the use of the h4
+    # function to provide an additional header above each output
+    # section.
+    mainPanel(
+      h4("Words per topic"),
+      tableOutput("view")
+    )
+  )
+))
